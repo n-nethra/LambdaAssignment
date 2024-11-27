313f9b62428d1b380ae108935ce3ff59dfde3f60
shish101 <shiyona.de@gmail.com>
2024-11-26 21:52:16 -0500

    Created an instance of the interface
    Created the instance of the interface in the Main.java and in the main method

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
-public class Main{
+public class Main {
+
+    public static void main(String[] args){
+
+        Addition sumInRange = (start, end) -> {
+            int sum = 0;
+
+            for (int i = start; i <= end; i++){
+                sum += i;
+            }
+
+            return sum;
+        };
+    }
 
 }
